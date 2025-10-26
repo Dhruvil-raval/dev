def5cb3784373f6bd3a345097a9a81dca27d2d97
Dhruvil-raval <dhruvilraval146@gmail.com>
2025-10-26 21:53:32 +0530

    Add functions to calculate sum and check service health

---
+++ your_app_code.py
@@ -0,0 +1,9 @@
+# your_app_code.py
+
+def calculate_sum(a, b):
+    """Returns the sum of two numbers."""
+    return a + b
+
+def is_service_healthy(status_code):
+    """Checks for a successful HTTP status."""
+    return status_code == 200
