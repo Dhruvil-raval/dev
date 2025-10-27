1d582592848614d41829a4d86fb69eb037245d96
Dhruvil-raval <dhruvilraval146@gmail.com>
2025-10-27 22:35:42 +0530

    Delete your_app_code.py

--- your_app_code.py
+++
@@ -1,9 +0,0 @@
-# your_app_code.py
-
-def calculate_sum(a, b):
-    """Returns the sum of two numbers."""
-    return a + b
-
-def is_service_healthy(status_code):
-    """Checks for a successful HTTP status."""
-    return status_code == 200
